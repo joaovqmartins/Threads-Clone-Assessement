c505ab078893ac211cdb45239ac46c62b74eca27
joaovqmartins <joavmartin3zp1n@gmail.com>
2024-09-23 23:16:09 -0300

    Commitando meu AT

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import threads from "./assets/threads.png";
+import layouts from "./assets/layouts.png";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav className="App-nav">
+          <img src={threads} alt="Icon-Threads" className="Img-icon" />
+          <p> Página inicial </p>
+          <img src={layouts} alt="Icon-Layouts" className="Img-icon" />
+        </nav>
       </header>
+      //começo do main
+      <main className="App-main">
+        <div className="App-card">
+
+        </div>
+      </main>
+      //começo do footer
+      <footer className="App-footer">
+        
+      </footer>
     </div>
   );
 }
